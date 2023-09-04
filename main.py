7b901473a86fb8e3adda02f3869d7a7a52dceb28
Hoon-Jung <hoonjung06@gmail.com>
2023-09-03 20:21:50 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
+
+import streamlit as st
+
+def prep_db():
+    df = pd.read_csv("./reviews/OFFICIAL_AMAZON_FASHION_TAGS.csv")
+    # docs = DataFrameLoader(df, page_content_column="reviewText").load()
+    # print(docs)
+
+    unique_products = df['asin'].unique()
+    for p in unique_products:
+        same_products = df[df["asin"]==p]["reviewText"]
+        reviews_doc = same_products.tolist()
+        reviews_doc = "\n".join(reviews_doc)
+        # print(reviews_doc)pip
+
+        text_splitter = CharacterTextSplitter(separator="\n",chunk_size=1000)
+        texts = text_splitter.create_documents([reviews_doc])
+
+        db = FAISS.from_documents(texts, HuggingFaceEmbeddings())
+        db.save_local(f"{p}")
+
+
+def get_answer(product_id, question):
+    selected_db = FAISS.load_local(f"db/{product_id}", HuggingFaceEmbeddings())
+
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa_chain = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=selected_db.as_retriever())
+    res = qa_chain({"query": question})
+    return res["result"]
+
+if __name__ == "__main__":
+    st.title("Review Search Engine")
+    options = ["B00007GDFV", "B00008JOQI", "7106116521"]
+    selected_option = st.selectbox("Select a product", options)
+    st.write("Currently selected:", selected_option)
+    question = st.text_input("Ask a question")
+    if st.button("Get Answer"):
+        st.write(get_answer(selected_option, question))
