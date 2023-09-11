58d71610089b41b323c0f93515167eae9733ca3b
Hoon-Jung <hoonjung06@gmail.com>
2023-09-10 21:53:34 -0400

    debugging error with document saving and embedding

--- main.py
+++ main.py
@@ -14,40 +14,39 @@ import os
 
 from langchain import PromptTemplate
 
-def prep_db():
+def prep_db(doc):
+
     df = pd.read_csv("./reviews/OFFICIAL_AMAZON_FASHION_TAGS.csv")
-    # docs = DataFrameLoader(df, page_content_column="reviewText").load()
-    # print(docs)
 
-    unique_products = df['asin'].unique()
-    for p in unique_products:
-        same_products = df[df["asin"]==p]["reviewText"]
-        reviews_doc = same_products.tolist()
-        reviews_doc = "\n".join(reviews_doc)
-        # print(reviews_doc)
+    # unique_products = df['asin'].unique()
+    same_products = df[df["asin"]==doc]["reviewText"]
+    reviews_doc = same_products.tolist()
+    reviews_doc = "\n".join(reviews_doc)
 
-        text_splitter = CharacterTextSplitter(separator="\n",chunk_size=1000)
-        texts = text_splitter.create_documents([reviews_doc])
+    text_splitter = CharacterTextSplitter(separator="\n",chunk_size=1000)
+    texts = text_splitter.create_documents([reviews_doc])
 
-        db = FAISS.from_documents(texts, HuggingFaceEmbeddings())
-        db.save_local(f"{p}")
+    db = FAISS.from_documents(texts, HuggingFaceEmbeddings())
+
+    return db
 
 
-def get_answer(product_id, question, apikey):
-    # selected_db = FAISS.load_local(f"db/{product_id}", HuggingFaceEmbeddings())
 
+def get_answer(product_id, question, apikey, selected_db):
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=apikey)
 
     # qa_chain = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=selected_db.as_retriever())
     # qa_chain = RetrievalQA.from_chain_type(llm)
     # res = llm({"query": question})
-    prompt_template = PromptTemplate.from_template(
-    "Tell me a funny joke about {content}."
-    )
-    chain = LLMChain(llm=llm, prompt=prompt_template)
-    res = chain.run(content=question)
 
-    return res
+
+    # prompt_template = PromptTemplate.from_template(
+    # "Tell me a funny joke about {content}."
+    # )
+    # chain = LLMChain(llm=llm, prompt=prompt_template)
+    # res = chain.run(content=question)
+
+    return selected_db
 
 if __name__ == "__main__":
     st.title("Review Search Engine")
@@ -68,4 +67,4 @@ if __name__ == "__main__":
     st.write("Currently selected:", selected_option)
     question = st.text_input("Ask a question")
     if st.button("Get Answer"):
-        st.write(get_answer(selected_option, question, openai_api_key))
+        st.write(get_answer(selected_option, question, openai_api_key, prep_db(selected_option)))
